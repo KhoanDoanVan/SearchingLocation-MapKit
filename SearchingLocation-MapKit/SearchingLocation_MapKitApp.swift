048e022badfc7d4fe1a0efe9d3c021b9ddc428e0
khoanisdev <doanvankhoan124@gmail.com>
2024-11-21 19:29:54 +0700

    Initial Commit

---
+++ SearchingLocation-MapKit/SearchingLocation_MapKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchingLocation_MapKitApp.swift
+//  SearchingLocation-MapKit
+//
+//  Created by Đoàn Văn Khoan on 21/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SearchingLocation_MapKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
